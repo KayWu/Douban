cae6880dbd634c00c7b69a99aa7c8d20dcd31f8e
Kay Wu <wuyingying.kay@gmail.com>
2015-01-26 21:10:06 +0800

    Add viewpager and tab

---
+++ app/src/main/java/com/kay/douban/MainActivity.java
@@ -0,0 +1,77 @@
+package com.kay.douban;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Window;
+
+import com.astuetz.PagerSlidingTabStrip;
+
+
+public class MainActivity extends FragmentActivity {
+
+    private PagerSlidingTabStrip pagerTab;
+    private ViewPager pager;
+    public final int NUM_PAGES = 4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        pagerTab = (PagerSlidingTabStrip) findViewById(R.id.pager_tabs);
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+        pagerTab.setViewPager(pager);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        float padding = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, dm);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (pager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the first page.
+            pager.setCurrentItem(0);
+        }
+    }
+    private class PagerAdapter extends FragmentPagerAdapter {
+
+        private final String[] TITLES = getResources().getStringArray(R.array.pager_name);
+
+        private PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Bundle bundle = new Bundle();
+            Fragment pagerFragment = new PagerFragment();
+            bundle.putInt("page_num", position);
+            pagerFragment.setArguments(bundle);
+            return pagerFragment;
+
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+}
---
+++ app/src/main/java/com/kay/douban/PagerFragment.java
@@ -0,0 +1,32 @@
+package com.kay.douban;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link android.app.Fragment} subclass.
+ */
+public class PagerFragment extends android.support.v4.app.Fragment {
+    private int pageNum;
+    private TextView tv;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View pager = inflater.inflate(R.layout.pager_fragment, container, false);
+        tv = (TextView) pager.findViewById(R.id.tv);
+        if (savedInstanceState == null) {
+            Bundle bundle = getArguments();
+            pageNum = bundle.getInt("page_num");
+            tv.setText("" + pageNum);
+        }
+        return pager;
+
+    }
+
+}
